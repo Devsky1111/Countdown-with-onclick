985f949e7f5b725449ffdcd66f38726191fc39a6
DevSky <trungphamk53@gmail.com>
2021-11-11 20:45:54 +0700

    them input time cho countdown

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import React from 'react'
 import { useState, useEffect, useRef } from 'react'
 function App() {
-  const [countdown, setCountdown] = useState(4000)
+  const [countdown, setCountdown] = useState(0)
+  const [valueInput, setvalueInput] = useState('')
   let id = useRef()
   function getCoundown(ms) {
 
@@ -11,19 +12,29 @@ function App() {
     let hours = Math.floor((ms % 86400) / 3600)
     let min = Math.floor((ms % 86400 % 3600) / 60)
     let second = ms % 86400 % 3600 % 60
-    console.log(day, hours, min, second)
+    // console.log(day, hours, min, second)
     return {
       day, hours, min, second
     }
   }
   console.log('re-render o ca day')
   function handleClick() {
+
+    clearInterval(id.current)
     id.current = setInterval(() => {
       setCountdown((prevcountdown) => {
-        getCoundown(prevcountdown - 1)
-        return prevcountdown - 1
+        if (prevcountdown === 1) {
+          clearInterval(id.current)
+          return 0
+        }
+
+        else {
+          getCoundown(prevcountdown - 1)
+          return prevcountdown - 1
+        }
+
       })
-      console.log(id)
+      // console.log(id)
     }, 1000)
   }
 
@@ -31,13 +42,30 @@ function App() {
     clearInterval(id.current)
   }
 
+  function handleChange(e) {
+    setCountdown(e.target.value)
+    setvalueInput(e.target.value)
+    // console.log('handleChange')
+  }
 
+  function handleSubmit(e) {
+    e.preventDefault()
+    handleClick()
+    setvalueInput('')
+
+  }
   return (
     <div>
       Bắt đầu đếm ngược: <span>{countdown} s</span>
       <div>
         <button onClick={handleClick}>Start</button>
         <button onClick={stophandle}>Stop</button>
+
+        <form onSubmit={handleSubmit}><div
+        >Nhập giá trị cần đếm ngược:</div>
+          <input type='text' placeholder='Nhập giá trị' onChange={handleChange} value={valueInput} ></input>
+          <button type='submit'> Submit Value</button>
+        </form>
 
       </div>
 
